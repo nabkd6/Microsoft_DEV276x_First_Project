b1a7b44f16c5f4444b95c5439bca300b3315df56
nabkd6 <nabkd6@mail.missouri.edu>
2019-10-07 15:07:44 +0100

    added "Hacker Problem" code for calculating distance

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
         budget();
         time();
         distance();
-
+        travelDistance();
     }
     public static void intro(){
         Scanner input = new Scanner(System.in);
@@ -38,7 +38,6 @@ public class Main {
         double convertedDailyBudget = convertedMoney / days;
         System.out.println("Your total budget in " + currency + " is $" + String.format("%.2f",convertedMoney) + " " + currency +
                 ", which per day is $" + String.format("%.2f",convertedDailyBudget) + " " + currency + ".");
-
     }
 
     public static void time(){
@@ -50,7 +49,6 @@ public class Main {
         System.out.println("That means that when it is midnight at home it will be " + ((timeFromMidnight % 24 + 24) % 24) +
                 ":00 in your travel destination and when it is noon at home it will be " + ((timeFromNoon % 24 + 24) % 24) +
                 ":00.\n***********");
-
     }
 
     public static void distance(){
@@ -60,8 +58,27 @@ public class Main {
         double km2MilesFactor = 1/2.58;
         int squareKm = (int) (squareMiles * km2MilesFactor);
         System.out.println("In miles2 that is " + squareKm + ".");
-
-
+    }
+    public static void travelDistance(){
+        /*From Wikipedia; Haversine formula is:
+        2r arcsin(sqrt(sin^2((lat2 - lat1)/2))+ cos(lat1)cos(lat2)sin^2((long2-long1)/2)) */
+        double earthRadius = 6371.8; /* radius of earth in km */
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is the latitude of your home? ");
+        double homeLat = input.nextDouble();
+        System.out.print("What is the longitude of your home? ");
+        double homeLong = input.nextDouble();
+        System.out.print("What is the latitude of your destination? ");
+        double destLat = input.nextDouble();
+        System.out.print("What is the longitude of your destination?");
+        double destLong = input.nextDouble();
+        double dLat = Math.toRadians(destLat - homeLat);
+        double dLong = Math.toRadians(destLong - homeLong);
+        double a = Math.pow(Math.sin(dLat / 2), 2) + Math.pow(Math.sin(dLong / 2), 2)
+                * Math.cos(Math.toRadians(homeLat)) * Math.cos(Math.toRadians(destLat));
+        double b = 2 * Math.asin(Math.sqrt(a));
+        double totalDistance = earthRadius * b;
+        System.out.println("The total distance between your home and destination is " + String.format("%.2f", totalDistance) + " km.");
     }
 
 
